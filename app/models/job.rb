22e175c47454efed47d03894e7274c5344546986
RISA <risa@RISAdeMacBook-Pro.local>
2017-04-16 08:06:18 +0800

    implement wage part

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   validates :title, presence: true
-  def admin?
-    is_admin
-  end
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
 end
