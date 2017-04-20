41129a2a4059b48761cade6ace05d55c29c891ee
RISA <risa@RISAdeMacBook-Pro.local>
2017-04-20 20:39:50 +0800

    step 6 change

--- app/models/job.rb
+++ app/models/job.rb
@@ -13,5 +13,5 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
   scope :published, -> { where(is_hidden: false) }
-  scope :recent, -> { order('created_t DESC') }
+  scope :recent, -> { order('created_at DESC') }
 end
