5188ffc18203d16eba804dc3750b675371675750
RISA <risa@RISAdeMacBook-Pro.local>
2017-04-22 17:32:10 +0800

    change welcome index

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -24,7 +24,7 @@ class Admin::JobsController < ApplicationController
   end
   def update
     @job = Job.find(params[:id])
-    if@job.update
+    if @job.update(job_params)
       redirect_to admin_jobs_path
     else
       render :edit
